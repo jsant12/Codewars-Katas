ce807f620a9b067c2d0b2e251de4b4c2e28ac3f1
Josue <jsant12@wgu.edu>
2020-10-25 10:31:28 -0400

    Even or Odd

---
+++ EvenOrOdd/EvenorOdd.js
@@ -0,0 +1,19 @@
+function even_or_odd(number) {
+    if (number % 2 === 0)
+      return "Even"
+    else
+      return "Odd"
+  }
+
+/* Best Practices
+
+  function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+  }
+
+*/
+/* Clever
+
+var even_or_odd = n => n & 1 ? 'Odd' : 'Even'
+
+*/
